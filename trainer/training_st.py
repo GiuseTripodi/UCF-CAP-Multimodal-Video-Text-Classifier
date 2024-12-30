e380b7d9fc70427729cd2751f00f4c9f696aa8db
GiuseTripodi <giuseppetripodi1@outlook.it>
2024-12-30 14:11:18 +0100

    implemented training_st.py

--- trainer/training_st.py
+++ trainer/training_st.py
@@ -10,8 +10,9 @@ transform = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
-csv_file = '/Users/user/PycharmProjects/frozen-in-time/data/UcfCap/dataset.csv'
-dataset = UCF101Dataset(csv_file, transform=transform)
+csv_train_file = '/Users/user/PycharmProjects/frozen-in-time/data/UcfCap/train_dataset.csv'
+dataset = UCF101Dataset(csv_train_file, transform=transform)
+
 dataloader = DataLoader(dataset, batch_size=16, shuffle=True)
 
 model = SpaceTimeTransformer(
@@ -32,13 +33,13 @@ model = model.to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=1e-4)
 
-num_epochs = 10
+num_epochs = 5
 
 for epoch in range(num_epochs):
     model.train()
     running_loss = 0.0
-    for inputs, labels in tqdm(dataloader):
-        #inputs, labels = inputs.to(device), labels.to(device)
+    for inputs, labels, *other_info in tqdm(dataloader):
+        inputs, labels = inputs.to(device), labels.to(device)
 
         # Forward pass
         outputs = model(inputs)
@@ -52,3 +53,8 @@ for epoch in range(num_epochs):
         running_loss += loss.item()
 
     print(f"Epoch {epoch+1}, Loss: {running_loss/len(dataloader)}")
+
+# Save the model's state dictionary
+save_path = 'spacetime_transformer_ucf101.pth'
+torch.save(model.state_dict(), save_path)
+print(f"Model saved to {save_path}")
