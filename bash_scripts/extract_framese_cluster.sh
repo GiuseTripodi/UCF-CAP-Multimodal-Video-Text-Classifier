f5ae0409c0a2f63a156e36adb08e2c06eebc68a8
GiuseTripodi <giuseppetripodi1@outlook.it>
2025-01-02 12:20:19 +0100

    New script to extract_framese_cluster.sh

--- bash_scripts/extract_framese_cluster.sh
+++ bash_scripts/extract_framese_cluster.sh
@@ -1,4 +1,5 @@
 mkdir /Users/user/PycharmProjects/frozen-in-time/data/UcfCap/UCF_frames
+export PATH=$PATH:/path/to/extracted/ffmpeg
 for video in /mnt/iusers01/mace01/t08341gt/UCF_cap_mh/data/UcfCap/YouTubeClips/*.mp4; do
     folder=/mnt/iusers01/mace01/t08341gt/UCF_cap_mh/data/UcfCap/YouTubeClips/$(basename "$video" .mp4)
     mkdir -p "$folder"
