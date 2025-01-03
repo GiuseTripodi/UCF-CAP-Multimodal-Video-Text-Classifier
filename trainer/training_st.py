d67e886de03f5fcac277371afb20ecaf01628314
GiuseTripodi <giuseppetripodi1@outlook.it>
2025-01-03 13:04:12 +0100

    Modified script to use pre-defined logger.py. Added script to train the model i the cluster

--- trainer/training_st.py
+++ trainer/training_st.py
@@ -1,5 +1,9 @@
-import argparse
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+
+import argparse
 import torch.optim as optim
 import torch.nn as nn
 from tqdm import tqdm
@@ -7,8 +11,6 @@ from model.video_transformer import *
 from data_loader.ucf_cap_dataset import *
 import logging
 from logger import setup_logging
-import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 # Configure and create a logger
