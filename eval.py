14120c14cb41300316eac8e0b65a4c35d68e0c14
GiuseTripodi <giuseppetripodi1@outlook.it>
2025-01-20 17:05:07 +0000

    Modified code to get information from a configuration file. Need to run the experiments

--- eval.py
+++ eval.py
@@ -87,6 +87,7 @@ def eval_spacetime(config: ConfigParser, model_name):
     # Data transformation
     transform = transforms.Compose([
         transforms.Resize((224, 224)),
+        transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
 
