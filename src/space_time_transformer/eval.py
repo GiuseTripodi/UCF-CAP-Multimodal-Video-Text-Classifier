b265e44822a2748d4ccabe334d612a82c67fd3e6
GiuseTripodi <giuseppetripodi1@outlook.it>
2025-10-28 11:50:33 +0000

    Modified eval function to be used.

--- src/space_time_transformer/eval.py
+++ src/space_time_transformer/eval.py
@@ -2,6 +2,9 @@ import sys
 import os
 from datetime import date
 from sklearn.metrics import precision_score, recall_score, f1_score, classification_report, confusion_matrix
+
+from data_loader.ucf_cap_loader import UCF101Dataset
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 import numpy as np
@@ -13,7 +16,6 @@ import numpy as np
 from torchvision import transforms
 from torch.utils.data import DataLoader
 from transformers import AutoImageProcessor, TimesformerForVideoClassification
-from data_loader.ucf_cap_dataset_embeddings import UCF101Dataset
 from src.utils.parse_config import ConfigParser
 
 
