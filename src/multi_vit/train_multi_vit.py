eb584236517239ef23af3bdeadeadaa44bf3040d
GiuseTripodi <giuseppetripodi1@outlook.it>
2025-11-14 11:11:32 +0000

    train multivit with interpolation and 32 framese

--- src/multi_vit/train_multi_vit.py
+++ src/multi_vit/train_multi_vit.py
@@ -45,8 +45,8 @@ def load_dataset(config):
         Resize((96, 96, 96)),
     ])
 
-    dataset_train = UCF101Dataset(csv_train_file_path, sampling_method='interpolate')
-    dataset_val = UCF101Dataset(csv_val_file_path, sampling_method='interpolate')
+    dataset_train = UCF101Dataset(csv_train_file_path, sampling_method='interpolate', num_frames=32)
+    dataset_val = UCF101Dataset(csv_val_file_path, sampling_method='interpolate', num_frames=32)
 
 
     train_loader = DataLoader(
